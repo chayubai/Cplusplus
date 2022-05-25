c15aa21bea50f7009a1b2216206d38662cf0cbe2
cha-yubai <2946053797@qq.com>
2022-05-25 10:16:23 +0800

    基础篇-指针引用

---
+++ 基础/1  hello world.cpp/10 指针引用/10 指针引用.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+struct teacher
+{
+	int id;
+	char name[20];
+};
+//二级指针
+int get_mem1(struct teacher **tpp)
+{
+	//tpp指向指针的指针，*tpp指针，指向的是Person本体，**tppPerson本体
+	//直接*tpp = (struct teacher*)malloc(sizeof(struct teacher));
+	struct teacher* tp = NULL;
+	tp = (struct teacher*)malloc(sizeof(struct teacher));
+	if (tp == NULL)
+	{
+		return -1;
+	}
+	tp->id = 100;
+	strcpy(tp->name, "lisi");
+
+	*tpp = tp;
+	return 0;
+}
+void free_teacher1(struct teacher** tpp)
+{
+	if (tpp == NULL)
+		return;
+	struct teacher* tp = *tpp;
+	if (tp != NULL)
+	{
+		free(tp);
+		*tpp = NULL;
+	}
+}
+//同级指针引用
+int get_mem2(struct teacher* &tp)//struct teacher* t = NULL;struct teacher* &tp = t
+{
+	tp = (struct teacher*)malloc(sizeof(struct teacher));
+	if (tp == NULL)
+	{
+		return -1;
+	}
+	tp->id = 100;
+	strcpy(tp->name, "wangwu");
+
+	return 0;
+}
+void free_teacher1(struct teacher* &tp)
+{
+	if (tp != NULL)
+	{
+		free(tp);
+		tp = NULL;
+	}
+}
+int main()
+{
+	struct teacher* t = NULL;
+	get_mem1(&t);
+	cout << "id = " << t->id << ",name = " << t->name << endl;
+	free_teacher1(&t);
+
+	get_mem2(t);
+	cout << "id = " << t->id << ",name = " << t->name << endl;
+
+	return 0;
+}
