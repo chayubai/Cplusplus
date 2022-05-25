0477b7e28a978b36632a8118e0ef600eb119d36b
cha-yubai <2946053797@qq.com>
2022-05-26 00:36:49 +0800

    基础篇-const分配内存情况

---
+++ 基础/1  hello world.cpp/11 const分配内存情况/11 const分配内存情况.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+//1、对const变量取地址，变量放在符号表中，会分配临时内存
+void test1()
+{
+	const int a = 10;
+	int* p = (int*)&a;
+}
+
+//2、使用普通变量，初始化const变量
+void test2()
+{
+	int a = 10;
+	const int b = a;
+	int* p = (int*)&a;
+	*p = 100;//修改成功
+	cout << "b = " << b << endl;
+}
+
+#include <string>
+//3、对于自定义数据类型
+struct Person
+{
+	string m_Name;//包含头文件#include <string>
+	int m_Age;
+};
+void test3()
+{
+	const Person p;
+	//p.m_Age = 10;直接修改失败
+	
+	//间接通过指针修改，成功
+	Person* pp = (Person*)&p;
+	(*pp).m_Name = "Tom";
+	pp->m_Age = 10;
+	cout << "姓名：" << p.m_Name << ",年龄：" << p.m_Age << endl;
+}
+int main()
+{
+	//test1();
+	//test2();
+	//test3();
+	return 0;
+}
