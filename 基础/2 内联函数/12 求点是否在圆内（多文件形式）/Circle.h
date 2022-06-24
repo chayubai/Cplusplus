ef85bcafb228581af59865acf4ba85beee4d119f
cha-yubai <2946053797@qq.com>
2022-06-24 21:54:34 +0800

    基础篇-求点是否在圆内（多文件形式）

---
+++ 基础/2 内联函数/12 求点是否在圆内（多文件形式）/Circle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Point.h"
+
+//‘≤¿‡
+class Circle
+{
+public:
+	void setXY(int x, int y);
+	
+	void setR(int r);
+	
+	bool is_in_Cricle(Point& p);
+	
+private:
+	int x0;
+	int y0;
+	int m_r;
+};
