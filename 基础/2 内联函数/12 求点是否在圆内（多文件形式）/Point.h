377358afca7db52d47067777641ca42af7b6ffd4
cha-yubai <2946053797@qq.com>
2022-06-24 21:55:37 +0800

    基础篇-求点是否在圆内（多文件形式）

---
+++ 基础/2 内联函数/12 求点是否在圆内（多文件形式）/Point.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//µ„¿‡
+class Point
+{
+public:
+	void setXY(int x, int y);
+	int getX();
+	int getY();
+private:
+	int m_x;
+	int m_y;
+};
+
+
