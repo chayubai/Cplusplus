39746eed77c1dc3507fee25681b743194067e59c
cha-yubai <2946053797@qq.com>
2022-07-05 20:22:54 +0800

    基础篇-强化练习

---
+++ 基础/4 全局函数和成员函数/2  强化练习/main.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "MyArray.h"
+
+int main()
+{
+	MyArray array1(10);//开辟10个元素的数组
+	//赋值操作
+	for (int i = 0; i < 10; i++)
+	{
+		array1.setData(i, i + 10);
+	}
+	cout << "------------" << endl;
+	cout << "array1:";
+	//打印
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array1.getData(i) << " ";
+	}
+	cout << endl;
+
+	MyArray array2 = array1;//array2的拷贝构造函数
+	cout << "array2:";
+
+	for (int i = 0; i < array2.getLen(); i++)
+	{
+		cout << array2.getData(i) << " ";
+	}
+	cout << endl;
+
+	MyArray array3;
+	array3 = array1;
+	cout << "array3:";
+
+	for (int i = 0; i < array3.getLen(); i++)
+	{
+		cout << array3.getData(i) << " ";
+	}
+	cout << endl;
+	return 0;
+}
