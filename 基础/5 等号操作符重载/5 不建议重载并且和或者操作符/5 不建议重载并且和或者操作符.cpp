a2e9adff095035cf2bac049e54fdea1ca0062806
cha-yubai <2946053797@qq.com>
2022-07-11 08:28:10 +0800

    基础篇-不建议重载并且和或者操作符

---
+++ 基础/5 等号操作符重载/5 不建议重载并且和或者操作符/5 不建议重载并且和或者操作符.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+class Complex
+{
+public:
+	Complex(int flag)
+	{
+		this->flag = flag;
+	}
+	Complex& operator+=(Complex& complex)
+	{
+		this->flag = this->flag + complex.flag;
+		return *this;
+	}
+	bool operator&&(Complex& complex)
+	{
+		return this->flag && complex.flag;
+	}
+public:
+	int flag;
+};
+int main()
+{
+	Complex complex1(0);//flag = 0
+	Complex complex2(1);//flag = 1
+
+	//失去了短路特性
+	//complex1.oprator&&(complex1.operator+=(complex2))
+	//先运算complex1 += complex2导致complex1为1，此时complex1 && complex1为真
+
+	//因此不要重载&&和||操作符
+	if (complex1 && (complex1 += complex2))
+	{
+		cout << "真！" << endl;
+	}
+	else
+	{
+		cout << "假！" << endl;
+	}
+
+	return 0;
+}
