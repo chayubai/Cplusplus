4abb51d4e1202fb0af4f7238858eb2feb5547723
cha-yubai <2946053797@qq.com>
2022-07-12 14:39:14 +0800

    基础篇-自定义字符串类

---
+++ 基础/5 等号操作符重载/7 自定义字符串类/main.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "MyString.h"
+
+int main()
+{
+#if 0
+	MyString s1("abc");//有参构造
+	MyString s2(s1);//拷贝构造
+	MyString s3 = "123";
+
+	cout << s1 << endl;
+	cout << s2 << endl;
+
+	s1[1] = 'X';//重载[]
+	cout << s1 << endl;
+
+	s1 = s3;//重载=
+	cout << s1 << endl;
+
+	cin >> s1;
+	cout << s1 << endl;
+#endif
+
+#if 0
+	MyString s4("abc");
+	cin >> s4;
+	cout << s4 << endl;
+#endif
+#if 0
+	MyString s1("123");
+	MyString s2("456");
+	cout << s1+s2 << endl;
+	cout << s1 << endl;
+	cout << s2 << endl;
+#endif
+
+	MyString s1("123");
+	MyString s2("456");
+	cout << (s1 + s2) << endl;
+	cout << s1 << endl;
+	cout << s2 << endl;
+
+	return 0;
+}
