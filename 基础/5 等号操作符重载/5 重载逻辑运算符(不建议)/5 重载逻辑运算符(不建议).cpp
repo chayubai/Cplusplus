f48fe7f75c973bc328562c59ec3a25feaf38ca24
cha-yubai <2946053797@qq.com>
2022-07-12 14:34:49 +0800

    基础篇-重载逻辑运算符(不建议)

---
+++ 基础/5 等号操作符重载/5 重载逻辑运算符(不建议)/5 重载逻辑运算符(不建议).cpp
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+class Test
+{
+public:
+	Test(int value)
+	{
+		this->value = value;
+	}
+	Test& operator+=(Test& another)
+	{
+		cout << "执行了+=操作符重载..." << endl;
+		Test temp(this->value + another.value);
+		return temp;
+	}
+	bool operator&&(Test& another)
+	{
+		cout << "执行了&&操作符重载..." << endl;
+		if (this->value && another.value)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool operator||(Test& another)
+	{
+		cout << "执行了||操作符重载..." << endl;
+		if (this->value || another.value)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	~Test()
+	{
+		cout << "~Test()..." << endl;
+	}
+private:
+	int value;
+};
+int main()
+{
+
+#if 0
+	int a = 0;
+	if (a && (a = 10))//&& 短路与
+	{
+
+	}
+	cout << "a = " << a << endl;
+	a = 1;
+	if (a || (a = 10))//|| 短路与
+	{
+
+	}
+	cout << "a = " << a << endl;
+#endif
+
+	Test t1(0);
+	Test t2(20);
+	//重载&&操作符，并不会发生短路与现象
+	if (t1 && (t1 += t2))//t1.operator&&( t1.operator+=(t2) )
+	{
+		cout << "为真" << endl;
+	}
+	else
+	{
+		cout << "为假" << endl;
+	}
+	cout << "-------------------------------" << endl;
+
+	//重载&&操作符，并不会发生短路或现象
+	if (t1 || (t1 += t2))//t1.operator||( t1.operator+=(t2) )
+	{
+		cout << "为真" << endl;
+	}
+	else
+	{
+		cout << "为假" << endl;
+	}
+	return 0;
+}
