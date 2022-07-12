d26303573dc74d9b0eb058a708fd6a6fae08761f
cha-yubai <2946053797@qq.com>
2022-07-12 14:45:30 +0800

    基础篇-继承的意义

---
+++ 基础/6 类与类之间的关系/1 继承的意义/1 继承的意义.cpp
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+#if 0
+class News
+{
+public:
+	void header()
+	{
+		cout << "公共的头部" << endl;
+	}
+	void footer()
+	{
+		cout << "公共的底部" << endl;
+	}
+	void leftList()
+	{
+		cout << "公共的左侧列表" << endl;
+	}
+	void content()
+	{
+		cout << "新闻播报..." << endl;
+	}
+};
+
+class Sport
+{
+public:
+	void header()
+	{
+		cout << "公共的头部" << endl;
+	}
+	void footer()
+	{
+		cout << "公共的底部" << endl;
+	}
+	void leftList()
+	{
+		cout << "公共的左侧列表" << endl;
+	}
+	void content()
+	{
+		cout << "世界杯赛况..." << endl;
+	}
+};
+#endif
+
+//利用继承模拟网页
+//继承的优点：减少重复代码，提供代码复用性
+class BasePage
+{
+public:
+	void header()
+	{
+		cout << "公共的头部" << endl;
+	}
+	void footer()
+	{
+		cout << "公共的底部" << endl;
+	}
+	void leftList()
+	{
+		cout << "公共的左侧列表" << endl;
+	}
+};
+
+//继承的语法：class 子类 : 继承方式 父类
+//News      子类   派生类
+//BasePage  父类   基类
+class News :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "新闻播报..." << endl;
+	}
+};
+
+class Sport :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "世界杯赛况..." << endl;
+	}
+};
+
+int main()
+{
+	News ns;
+	ns.header();
+	ns.footer();
+	ns.leftList();
+	ns.content();
+
+	Sport sp;
+	sp.header();
+	sp.footer();
+	sp.leftList();
+	sp.content();
+}
