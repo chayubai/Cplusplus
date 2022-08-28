423afd659f08db14fd6fd510e7dcd53f08c49330
cha-yubai <2946053797@qq.com>
2022-08-29 07:21:55 +0800

    基础篇-面向抽象类编程  案例2 - 动物园(多文件编写)

---
+++ 基础/7 多态的原理/12 面向抽象类编程  案例2 - 动物园/Cat.cpp
@@ -0,0 +1,15 @@
+#include "Cat.h"
+Cat::Cat()
+{
+	cout << "Cat()...." << endl;
+}
+
+Cat::~Cat()
+{
+	cout << "~Cat()...." << endl;
+}
+
+void Cat::voice()
+{
+	cout << "Cat: miaomiao ~666666" << endl;
+}
