f85166cd8fa876b1bd01723ca9379ab9a2cec493
cha-yubai <2946053797@qq.com>
2022-08-29 07:23:00 +0800

    基础篇-面向抽象类编程  案例2 - 动物园(多文件编写)

---
+++ 基础/7 多态的原理/12 面向抽象类编程  案例2 - 动物园/Dog.cpp
@@ -0,0 +1,15 @@
+#include "Dog.h"
+Dog::Dog()
+{
+	cout << "Dog()..." << endl;
+}
+
+Dog::~Dog()
+{
+	cout << "~Dog()..." << endl;
+}
+
+void Dog::voice()
+{
+	cout << "Dog: wangwang 55555~~" << endl;
+}
