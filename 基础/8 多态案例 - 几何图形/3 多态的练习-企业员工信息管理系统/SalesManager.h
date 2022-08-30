b03c46628ef2691d7793a25c9f0e585c0339d980
cha-yubai <2946053797@qq.com>
2022-08-31 07:57:15 +0800

    基础篇-多态的练习-企业员工信息管理系统

---
+++ 基础/8 多态案例 - 几何图形/3 多态的练习-企业员工信息管理系统/SalesManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Employee.h"
+#include "Manager.h"
+#include "SaleMan.h"
+
+class SalesManager : public Manager, public SaleMan
+{
+public:
+	SalesManager();
+	~SalesManager();
+
+	//得到员工的薪水的方法
+	virtual void getPay();
+
+	//员工的升级方法
+	virtual void uplevel(int addLevel);
+
+	virtual void init();
+
+};
+
+
