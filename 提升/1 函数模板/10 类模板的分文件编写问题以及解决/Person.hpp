afeeffd92fb580af7f4b54109d8d2e84d64d1170
cha-yubai <2946053797@qq.com>
2022-09-04 23:59:31 +0800

    提升篇-类模板的分文件编写问题以及解决

---
+++ 提升/1 函数模板/10 类模板的分文件编写问题以及解决/Person.hpp
@@ -0,0 +1,37 @@
+#pragma once
+//等价下面写法
+#ifndef PERSON_H//CTRL + SHIFT + U	将小写替换为大写
+
+#define PERSON_H
+//...
+
+#endif
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+template<class T1, class T2>
+class Person
+{
+public:
+	Person(T1 name, T2 age);
+
+	void showPerson();
+
+	T1 m_Name;
+	T2 m_Age;
+};
+
+template<class T1, class T2>
+Person<T1, T2>::Person(T1 name, T2 age)
+{
+	this->m_Name = name;
+	this->m_Age = age;
+}
+
+template<class T1, class T2>
+void Person<T1, T2>::showPerson()
+{
+	cout << "姓名：" << this->m_Name << "，年龄：" << this->m_Age << endl;
+}
